401373bad320c6f53a431fafc23791280c2b885c
vlasenkoi <159702406+vlasenkoi@users.noreply.github.com>
2026-04-28 03:15:22 +0300

    Add files via upload

---
+++ lab1.net/Shop.Common/Extensions.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Shop.Common
+{
+    public static class Extensions
+    {
+        public static void Print(this Product product)
+        {
+            Console.WriteLine(product.Name);
+        }
+    }
+}
